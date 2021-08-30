b09ac7d1555bca2ccc902eb8aa4e663c476969cf
EthanDP <etdpeck@gmail.com>
2021-08-30 13:21:36 -0500

    Cleanup, move code into correctly named directoy, use defines in notes.h instead of variables

---
+++ src/simon/notes.h
@@ -0,0 +1,17 @@
+#ifndef NOTES_HEADER
+#define NOTES_HEADER
+
+#define NOTE_C 239
+#define NOTE_CS 225
+#define NOTE_D  213
+#define NOTE_DS 201
+#define NOTE_E  190
+#define NOTE_F  179
+#define NOTE_FS 169
+#define NOTE_G  159
+#define NOTE_GS 150
+#define NOTE_A  142
+#define NOTE_AS 134
+#define NOTE_B  127
+
+#endif
